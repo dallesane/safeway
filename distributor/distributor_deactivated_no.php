baf9fcfb6497e62a402d622ed827b6fedc54e425
Mac <papa@Sumeets-MacBook.local>
2016-11-19 00:32:44 +0545

    total numbers & total activated number in array saved

--- distributor/distributor_deactivated_no.php
+++ distributor/distributor_deactivated_no.php
@@ -37,7 +37,23 @@
 
                 $result = mysql_query($query);
                 $total_number = array();
-                 // echo "hello";
+                $total_activate_no = array();
+
+                $qu = "SELECT * FROM activate_no WHERE distributor='$distributor_id'";
+                $res = mysql_query($qu);
+                // $total_activate_no = mysql_fetch_array($res);
+                $cnt = 0;
+                while($row4 = mysql_fetch_array($res)){
+
+                    $activate_no_detail = array();
+                    $activate_no_detail["phone_number"] = $row4['phone_no'];
+                    $activate_no_detail["district"] = $row4['district'];
+                    $activate_no_detail["date"] = $row4['date'];
+                    $total_activate_no[$cnt] = $activate_no_detail;
+                    $cnt = $cnt + 1;
+
+                }
+
 
                 while($row=mysql_fetch_array($result)){
 
@@ -45,20 +61,27 @@
                     $to = $row['phone_number_to_range'];
                     $date = $row['date'];
                     $district = $row['district'];
+                    
+
 
                     
                     $counter = 0;
                     while($from <= $to){
                         // echo $from;
                         // echo "<br>";
+                        $number_detail = array();
+                        $number_detail["phone_number"] = $from;
+                        $number_detail["district"] = $district;
+                        $number_detail["date"] = $date;
                         
+
 
                         // echo "<tr>";
                         // echo "<td>".$date."</td>";
                         // echo "<td>".$from."</td>";
                         // echo "<td>".$district."</td>";
                         // echo "</tr>";
-                        $total_number[$counter] = $from;
+                        $total_number[$counter] = $number_detail;
                         $from = $from + 1;
 
                         $counter = $counter + 1;
@@ -67,7 +90,8 @@
                     
                       
                 } 
-                print_r($total_number);
+                // print_r($total_number);
+                print_r($total_activate_no);
 
             ?>
 
