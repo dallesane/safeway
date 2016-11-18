7b8c64178b88a2f4843171df6a36f5f1b903af0c
Mac <papa@Sumeets-MacBook.local>
2016-11-18 10:53:27 +0545

    total numbers of each distributor displaying in table working

--- distributor/distributor_deactivated_no.php
+++ distributor/distributor_deactivated_no.php
@@ -1,5 +1,86 @@
 <?php
+    include("../connect_db.php");
+    function getdistributortotalnumber($id) {
+    	$query = "SELECT * FROM sim_details WHERE distributor='$id'";
+    	$result = mysql_query($query);
 
-    echo "Total deactivated numbers of distributor";
+    	$total_number = 0;
+    	while ($row=mysql_fetch_array($result)){
 
-?>    
+    		$from = $row['phone_number_from_range'];
+            $to = $row['phone_number_to_range'];
+        
+            $total_number += $to - $from + 1;
+
+
+    	}
+    	return $total_number;
+
+    }
+?>
+<html>
+		<style>
+			table {
+			    font-family: arial, sans-serif;
+			    border-collapse: collapse;
+			    width: 100%;
+			}
+
+			td, th {
+			    border: 1px solid #dddddd;
+			    text-align: left;
+			    padding: 8px;
+			}
+
+			tr:nth-child(even) {
+			    background-color: #dddddd;
+			}
+		</style>			
+		<body>
+
+			<table>
+			  <tr>
+			    <th>id</th>
+			    <th>distributor</th>
+			    <th>total numbers<</th>
+			    <th>total activated numbers</th>
+			    <th>total deactivated numbers</th>
+			  </tr> 
+	    	
+	   	
+	    	<?php
+	    	    $query = "SELECT * FROM distributor";
+
+		        $result = mysql_query($query);
+		  
+
+			    if ($result) {
+
+			        while($row = mysql_fetch_array($result)) {
+	    	            echo '<tr>';
+	    	            echo '<td>'. $row['id'].'</td>';
+	    	            echo '<td>'. $row['distributor'].'</td>';
+	    	            $distributor_id = $row['id'];
+	    	            $total_number = getdistributortotalnumber($distributor_id);
+	    	            echo '<td>'. $total_number .' <a href="distributor_total_no.php/?distributor_id='.$distributor_id.'"><button>Click for details!</button></a></td>';
+	    	            // echo "sumeet";
+	    	            
+	    	            $qu = "SELECT * FROM activate_no WHERE distributor='$distributor_id'";
+	    	            $res = mysql_query($qu);
+	    	            $activate_count = mysql_num_rows($res);
+
+	    	            // $activate_count = mysql_num_rows(mysql_query("SELECT * FROM activate_no WHERE 'distributor' = '$row['id'] "));
+	    	            echo '<td><a>'. $activate_count .' <a href="distributor_activated_no.php/?distributor_id='.$distributor_id.'"><button>Click for details!</button></a></td>';
+	    	            $deactivate_no = $total_number - $activate_count;
+	    	            echo '<td>'. $deactivate_no  .'  <a href="distributor_deactivated_no.php/?distributor_id='.$distributor_id.'"><button>Click for details!</button></a></td>';
+	    	            echo '</tr>';
+
+	    	        }
+	    	    }  
+	  			  	
+	    	?>
+	    		
+	    </table>
+	</body>
+
+</html> 
--- distributor/distributor_total_no.php
+++ distributor/distributor_total_no.php
@@ -1,5 +1,77 @@
-<?php
+<html> 
+<body> 
 
-    echo "work to do !!";
+        <style>
+            table {
+                font-family: arial, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+            }
 
-?>
+            td, th {
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+            }
+
+            tr:nth-child(even) {
+                background-color: #dddddd;
+            }
+        </style>    
+
+        <div class="container">   
+        <table class="table stripped">
+            
+                <tr>
+                <th>Issued date</th>
+                <th>phone_no</th>
+                <th>disrict</th>
+                </tr>
+
+
+
+        <?php
+           include("../connect_db.php");
+           $distributor_id = $_GET['distributor_id'];
+            
+                $query = "SELECT * FROM sim_details WHERE distributor='$distributor_id'";
+
+                $result = mysql_query($query);
+
+                 // echo "hello";
+
+                while($row=mysql_fetch_array($result)){
+
+                    $from = $row['phone_number_from_range'];
+                    $to = $row['phone_number_to_range'];
+                    $date = $row['date'];
+                    $district = $row['district'];
+                
+                    while($from <= $to){
+                        // echo $from;
+                        // echo "<br>";
+                        
+
+                        echo "<tr>";
+                        echo "<td>".$date."</td>";
+                        echo "<td>".$from."</td>";
+                        echo "<td>".$district."</td>";
+                        echo "</tr>";
+
+                        $from = $from + 1;
+
+                    }
+                    
+                      
+
+
+                } 
+
+        ?>
+
+
+        </table>
+        </div>  
+
+</body>        
+</html>
